a5e89dfd38df93daa6bce891f4e0cb737978bb10
Millie Kembe <mikembe@unc.edu>
2023-04-18 19:31:47 -0400

    updates 3

--- Todoist/Todoist/Views/TaskInfoView.swift
+++ Todoist/Todoist/Views/TaskInfoView.swift
@@ -10,18 +10,45 @@ import SwiftUI
 struct TaskInfoView: View {
     
     @Binding var task: Task
+    @State var isShowing = false
     
     var body: some View {
         NavigationStack {
             VStack {
                 HStack {
                     Image(systemName: "circle")
-                    TextField("\(task.name)", text: $task.name)
+                    Button {
+                        isShowing.toggle()
+                    } label: {
+                        Text("\(task.name)")
+                            .foregroundColor(.black).bold()
+                    }
+                    .sheet(isPresented: $isShowing) {
+                        VStack {
+                            HStack {
+                                Image(systemName: "circle")
+                                TextField("\(task.name)", text: $task.name)
+                            }
+                            HStack {
+                                Image(systemName: "text.alignleft")
+                                TextField("\(task.description ?? "Description")", text: $task.description.toUnwrapped(defaultValue: ""))
+
+                            }
+                        }
+                        .presentationDetents([.fraction(0.75)])
+
+                    }
+                    Spacer()
                     
                 }
+                .padding(.leading, 10)
                 HStack {
-                    Text("Due Date: ")
+                    Image(systemName: "sidebar.squares.left")
+                        .foregroundColor(.green)
+                    Text("April 15")
+                    Spacer()
                 }
+                .padding(.leading, 10)
                 HStack {
                     TextField("\(task.description ?? "Description")", text: $task.description.toUnwrapped(defaultValue: ""))
                 }
